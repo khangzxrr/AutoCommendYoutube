1dfd0707bf6580f19edf2906d988179fadd2cb14
khangzxrr <khangzxrr@gmail.com>
2021-04-04 14:22:20 +0700

    add new test

--- AutoCommendTest/UnitTest1.cs
+++ AutoCommendTest/UnitTest1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using AutoYoutube;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
 namespace AutoCommendTest
@@ -6,9 +7,13 @@ namespace AutoCommendTest
     [TestClass]
     public class UnitTest1
     {
+       
+
         [TestMethod]
-        public void TestMethod1()
+        public void TestAppStartupWithNoException()
         {
+            Form1 form = new Form1();
         }
+
     }
 }
